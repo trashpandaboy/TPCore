bc357f7daf566211822a2326071828d8361161d6
William Soro <william.soro@hotmail.it>
2023-08-26 13:44:38 +0200

    Added first implementation of Manager

---
+++ Manager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace com.trashpandaboy.unitycoreutils
+{
+    public class Manager : MonoBehaviour
+    {
+        public static Manager Instance
+        {
+            get { return _instance; }
+        }
+        static Manager _instance = null;
+
+        private void Awake()
+        {
+            if(_instance != null && _instance != this)
+            {
+                Destroy(this);
+            }
+            else
+            {
+                _instance = this;
+            }
+        }
+    }
+}
