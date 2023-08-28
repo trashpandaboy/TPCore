f7480540fa76f78bb0c6897e122d354e5991e373
William Soro <william.soro@hotmail.it>
2023-08-29 00:36:23 +0200

    Added generic required to correctly work with inheritance

--- Manager.cs
+++ Manager.cs
@@ -2,23 +2,23 @@ using UnityEngine;
 
 namespace com.trashpandaboy.core
 {
-    public class Manager : MonoBehaviour
+    public class Manager<T> : MonoBehaviour where T : Manager<T>
     {
-        public static Manager Instance
+        public static T Instance
         {
             get { return _instance; }
         }
-        static Manager _instance = null;
+        static T _instance;
 
         protected virtual void Awake()
         {
-            if(_instance != null && _instance != this)
+            if(_instance != null && _instance != (T)this)
             {
                 Destroy(this);
             }
             else
             {
-                _instance = this;
+                _instance = (T)this;
             }
         }
     }
