c30755aaf61971e6ffba7274756c14b6633536b8
William Soro <william.soro@hotmail.it>
2023-11-02 18:11:05 +0100

    Singleton is now optional in Manager (not in use as default)

--- Manager.cs
+++ Manager.cs
@@ -4,21 +4,24 @@ namespace com.trashpandaboy.core
 {
     public class Manager<T> : MonoBehaviour where T : Manager<T>
     {
-        public static T Instance
-        {
-            get { return _instance; }
-        }
+        [SerializeField]
+        bool UseSingleton = false;
+
+        public static T Instance => _instance;
         protected static T _instance;
 
         protected virtual void Awake()
         {
-            if(_instance != null && _instance != (T)this)
+            if(UseSingleton)
             {
-                Destroy(this);
-            }
-            else
-            {
-                _instance = (T)this;
+                if(_instance != null && _instance != (T)this)
+                {
+                    Destroy(this);
+                }
+                else
+                {
+                    _instance = (T)this;
+                }
             }
         }
     }
